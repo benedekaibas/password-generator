5ad318ad5837173baf26fd52316f0bcdbf7f4208
kaibas01 <kaibas01@allegheny.edu>
2023-10-07 19:23:32 -0400

    added comments

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+#lists that we can get the data from 
 letters = ["A", 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 symbols = ['`','~','!','@','#','$','%','^','&','*','(',')','_','-','+','=','{','[','}','}','|','<',',','>','.','?','/']
+#converting letters to string so we can lower the letters
 def toString():
     letters_string = str(letters)
     small_letters = letters_string.lower()
     return small_letters
-
+#ensure that the function toString works 
 print(toString())
+
+
+
 
 
 
