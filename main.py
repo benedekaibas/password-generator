2cf381d459f98cf638eba9bc0e279750d9a67ff9
kaibas01 <kaibas01@allegheny.edu>
2023-10-07 19:10:01 -0400

    started with symbols and letters

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+letters = ["A", 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+symbols = ['`','~','!','@','#','$','%','^','&','*','(',')','_','-','+','=','{','[','}','}','|','<',',','>','.','?','/']
+def toString():
+    letters_string = str(letters)
+    small_letters = letters_string.lower()
+    return small_letters
+
+print(toString())
+
+
+
+
+
 
